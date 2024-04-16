6d3e112adec9bbf5912eeb239d75126810760418
Trung Anh <anhthtservice@gmail.com>
2024-04-17 04:11:27 +0700

    Create react app

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react/prop-types": "off",
+    "react-refresh/only-export-components": "off",
+    "react/no-unescaped-entities": "off",
+    "no-unused-vars": "off",
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [],
+      },
+    ],
+  },
+};
